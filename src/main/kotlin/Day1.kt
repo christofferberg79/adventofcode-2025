204b5bd8bdef96e7d07c814d8c1705018a0e76ae
Christoffer Berg <christofferberg79@gmail.com>
2025-12-02 18:55:37 +0100

    Day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,7 +2,41 @@ package cberg.aoc2025
 
 class Day1(input: Input) {
 
-    fun part1() = 0
+    private data class Instruction(val dir: Char, val dist: Int)
 
-    fun part2() = 0
+    private val instructions = input.lines().map { Instruction(it.first(), it.drop(1).toInt()) }
+
+    private var count = 0
+    private var dial = 50
+
+    fun part1(): Int {
+        for (i in instructions) {
+            if (i.dir == 'L') {
+                dial = (dial - i.dist).mod(100)
+            } else {
+                dial = (dial + i.dist).mod(100)
+            }
+            if (dial == 0) {
+                count++
+            }
+        }
+        return count
+    }
+
+    fun part2(): Int {
+        for (i in instructions) {
+            if (i.dir == 'L') {
+                if (dial == 0) {
+                    count += i.dist / 100
+                } else if (i.dist >= dial) {
+                    count += (i.dist - dial) / 100 + 1
+                }
+                dial = (dial - i.dist).mod(100)
+            } else {
+                count += (i.dist + dial) / 100
+                dial = (dial + i.dist).mod(100)
+            }
+        }
+        return count
+    }
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -6,9 +6,9 @@ import kotlin.test.assertEquals
 class Test {
     @Test
     fun day1() {
-        assertEquals(0, Day1(Input("day1_ex1")).part1())
-        assertEquals(0, Day1(Input("day1")).part1())
-        assertEquals(0, Day1(Input("day1_ex1")).part2())
-        assertEquals(0, Day1(Input("day1")).part2())
+        assertEquals(3, Day1(Input("day1_ex1")).part1())
+        assertEquals(980, Day1(Input("day1")).part1())
+        assertEquals(6, Day1(Input("day1_ex1")).part2())
+        assertEquals(5961, Day1(Input("day1")).part2())
     }
 }
